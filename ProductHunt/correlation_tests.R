73526393eba130d2a9f0b7bb383951f0489e10ab
Nadeen Saleh <nsaleh2013@gmail.com>
2016-10-07 00:14:14 -0400

    Product Hunt analysis

---
+++ ProductHunt/correlation_tests.R
@@ -0,0 +1,65 @@
+.libPaths( c( .libPaths(), "~/REU-SOS/Stats/src/lib") )
+
+library(minerva)
+library(ggplot2)
+library(reshape2)
+library(RMySQL)
+library(lsr)
+
+# Get data from a database
+connection <- dbConnect(MySQL(), user = 'root', password = '', host = '127.0.0.1', dbname='ProductHunt')
+
+# Correlation between votes and days of week/hour
+f.query <- dbSendQuery(connection, "SELECT id, comments_count, DAYOFWEEK(created_at), HOUR(created_at) FROM Posts where comments_count>100")
+f.results = fetch(f.query, n=-1)
+comments  = f.results[,2]
+dayOfWeek = f.results[,3]
+hour = f.results[,4]
+
+cor.test(comments, dayOfWeek)
+cor.test(comments, hour)
+
+# Print out information from histogram
+info <- hist(f.results[,2])
+info
+
+# This is a neat trick to print out a histogram for all attributes of your data
+allHistograms <- melt(f.results[,c(2:4)])
+ggplot(allHistograms,aes(x = value)) +
+facet_wrap(~variable,scales = "free_x") +
+geom_histogram()
+
+# Get data from a database
+connection <- dbConnect(MySQL(), user = 'root', password = '', host = '127.0.0.1', dbname='ProductHunt')
+
+# Correlation between votes and days of week/hour
+f.query <- dbSendQuery(connection, "SELECT id, votes_count, DAYOFWEEK(created_at), HOUR(created_at) FROM Posts where votes_count>100")
+f.results = fetch(f.query, n=-1)
+votes  = f.results[,2]
+dayOfWeek = f.results[,3]
+hour = f.results[,4]
+
+cor.test(votes, dayOfWeek)
+cor.test(votes, hour)
+
+allHistograms <- melt(f.results[,c(2:4)])
+ggplot(allHistograms,aes(x = value)) +
+facet_wrap(~variable,scales = "free_x") +
+geom_histogram()
+
+connection <- dbConnect(MySQL(), user = 'root', password = '', host = '127.0.0.1', dbname='ProductHunt')
+
+# Correlation between votes and days of week/hour
+f.query <- dbSendQuery(connection, "SELECT id, COUNT(*) as postCount, DAYOFWEEK(created_at) as day, HOUR(created_at) as hour FROM Posts GROUP BY day,hour")
+f.results = fetch(f.query, n=-1)
+posts  = f.results[,2]
+dayOfWeek = f.results[,3]
+hour = f.results[,4]
+
+cor.test(posts, dayOfWeek)
+cor.test(posts, hour)
+
+allHistograms <- melt(f.results[,c(2:4)])
+ggplot(allHistograms,aes(x = value)) +
+facet_wrap(~variable,scales = "free_x") +
+geom_histogram()
---
+++ ProductHunt/queries.sql
@@ -0,0 +1,14 @@
+create table posts_by_day ( day int, count int);
+INSERT INTO posts_by_day SELECT DAYOFWEEK(created_at), COUNT(*) as timesCreated from posts GROUP BY DAYOFWEEK(created_at);
+create table users_by_day ( day int, count int);
+INSERT INTO users_by_day SELECT DAYOFWEEK(created_at), COUNT(*) as timesCreated from users GROUP BY DAYOFWEEK(created_at);
+select * from posts_by_day;
+select * from users_by_day;
+select posts_count, maker_of_count, followers_count from users;
+select posts_count, maker_of_count, followers_count from users ORDER BY followers_count DESC LIMIT 25;
+select posts_count, maker_of_count, followers_count, votes_count from users ORDER BY followers_count DESC LIMIT 25;
+select user_username,COUNT(*) as NumPosts from posts ORDER BY NumPosts DESC LIMIT 10;
+select user_username,COUNT(*) as NumPosts from posts GROUP BY user_username ORDER BY NumPosts DESC LIMIT 10;
+select tagline, votes_count from posts WHERE votes_count >= 38 Order By votes_count DESC LIMIT 40000;
+select name,followers_count,posts_count from topics Order By followers_count DESC limit 10;
+select name,followers_count,posts_count from topics Order By posts_count DESC limit 10;
